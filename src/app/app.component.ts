25944b39385349a76516c759885593885d3cc82b
Mile Jankuloski <mile.jankuloski@students.finki.ukim.mk>
2020-11-06 21:55:30 +0100

    v0.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Korona } from './shared/interfaces';
 export class AppComponent implements OnInit {
   now = Date.now();
   nowCD = Date.now();
+  last7DaysDate = Date.now();
   stats: string[];
   statscounter = 0;
   counter = 0;
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit {
   date: string;
   year: string;
   apidate: string;
+  apidatel7d: string;
   flag1 = 0;
   flag2 = 0;
   model: Korona = {};
@@ -26,13 +28,33 @@ export class AppComponent implements OnInit {
   mk: Korona = {};
   svet: Korona = {};
   selectedValue: Korona;
+  mk7days: any[] = [];
+  svet7days: any[] = [];
+  checker: string;
 
   constructor(private dataService: DataService) { 
 
    }
  
   ngOnInit() {
-      this.now = this.now;
+    this.checker = formatDate(this.now - 259200000, "dd.MM", "en-US");
+    if(this.checker[0] === '0') {
+      this.checker = this.checker.substring(1);
+    }
+    if(localStorage.getItem("date") === this.checker) {
+      localStorage.clear();
+      console.log("Clearing cache..")
+    }
+    if (localStorage.getItem("selectedValue") != null && localStorage.getItem("mk") != null && localStorage.getItem("svet") != null && localStorage.getItem("gradovi") != null && localStorage.getItem("date") != null && localStorage.getItem("mk7days") != null && localStorage.getItem("svet7days") != null) {
+      this.selectedValue = JSON.parse(localStorage.getItem("selectedValue"));
+      this.mk = JSON.parse(localStorage.getItem("mk"));
+      this.svet = JSON.parse(localStorage.getItem("svet"));
+      this.gradovi = JSON.parse(localStorage.getItem("gradovi"));
+      this.date = localStorage.getItem("date");
+      this.mk7days = JSON.parse(localStorage.getItem("mk7days"));
+      this.svet7days = JSON.parse(localStorage.getItem("svet7days"));
+    } else {
+      this.now = this.now - 86400000;
       this.year = formatDate(this.now, "yyyy", "en-US");
       this.month = formatDate(this.now, "MM", "en-US");
       this.date = formatDate(this.now, "dd.MM", "en-US");
@@ -42,11 +64,11 @@ export class AppComponent implements OnInit {
       this.retrievedata();
       this.apidate = formatDate(this.nowCD, "yyyy-MM-dd", "en-US");
       this.retrieveCovidData(this.apidate);
+    }
   }
 
   retrievedata() {
-    //this.dataService.readPdf('https://cors-anywhere.herokuapp.com/http://iph.mk/wp-content/uploads/'+this.year+'/'+this.month+'/'+this.date+'.pdf')
-    this.dataService.readPdf('assets/4.11.pdf')
+    this.dataService.readPdf('https://cors-anywhere.herokuapp.com/http://iph.mk/wp-content/uploads/'+this.year+'/'+this.month+'/'+this.date+'.pdf')
       .then(text => {
         text = text.slice(text.indexOf("Активни случаи (вкупно) ")+24, text.indexOf("   Вкупно "));
         this.stats = text.replace("Крива Паланка", "Крива-Паланка").replace("Демир Хисар", "Демир-Хисар").replace("Македонски Брод", "Македонски-Брод").replace("Свети Николе","Свети-Николе")
@@ -107,44 +129,92 @@ export class AppComponent implements OnInit {
       }
     }
     
-    if(this.gradovi[28]) {
-      this.selectedValue = this.gradovi[28];
+    if(this.gradovi[0]) {
+      this.selectedValue = this.gradovi[0];
     }
+    localStorage.setItem("date", this.date);
+    localStorage.setItem("gradovi", JSON.stringify(this.gradovi));
+    localStorage.setItem("selectedValue", JSON.stringify(this.selectedValue));
   }
 
   retrieveCovidData(apidate: string) {
     this.dataService.getCovidData(apidate)
-    .subscribe((data: any) => {
-      if(data.dates[apidate].countries['North Macedonia'].today_new_confirmed == 0) {
+      .subscribe((data: any) => {
+        if(data.dates[apidate].countries['North Macedonia'].today_new_confirmed == 0) {
+          this.nowCD = this.nowCD - 86400000;
+          this.apidate = formatDate(this.nowCD, "yyyy-MM-dd", "en-US");
+          this.retrieveCovidData(this.apidate);
+        }
+        else {
+          this.mk.novozaboleni = data.dates[apidate].countries['North Macedonia'].today_new_confirmed;
+          this.mk.novopochinati = data.dates[apidate].countries['North Macedonia'].today_new_deaths;
+          this.mk.novoozdraveni = data.dates[apidate].countries['North Macedonia'].today_new_recovered;
+          this.mk.vkupnozaboleni = data.dates[apidate].countries['North Macedonia'].today_confirmed;
+          this.mk.vkupnopochinati = data.dates[apidate].countries['North Macedonia'].today_deaths;
+          this.mk.vkupnoozdraveni = data.dates[apidate].countries['North Macedonia'].today_recovered;
+          this.mk.aktivni = data.dates[apidate].countries['North Macedonia'].today_open_cases;
+          this.mk.date = data.dates[apidate].countries['North Macedonia'].date;
+
+          this.svet.novozaboleni = data.total.today_new_confirmed;
+          this.svet.novopochinati = data.total.today_new_deaths;
+          this.svet.novoozdraveni = data.total.today_new_recovered;
+          this.svet.vkupnozaboleni = data.total.today_confirmed;
+          this.svet.vkupnopochinati = data.total.today_deaths;
+          this.svet.vkupnoozdraveni = data.total.today_recovered;
+          this.svet.aktivni = data.total.today_open_cases;
+          this.svet.date = data.total.date;
+
+          this.mk7days.push(this.mk);
+          this.svet7days.push(this.svet);
+          localStorage.setItem("mk", JSON.stringify(this.mk));
+          localStorage.setItem("svet", JSON.stringify(this.svet));
+          this.retrieveCovidDataLast7Days(this.nowCD);
+        }
+      },
+      (err: any) => {
+        console.log(err);
         this.nowCD = this.nowCD - 86400000;
         this.apidate = formatDate(this.nowCD, "yyyy-MM-dd", "en-US");
         this.retrieveCovidData(this.apidate);
-      }
-      else {
-        this.mk.novozaboleni = data.dates[apidate].countries['North Macedonia'].today_new_confirmed;
-        this.mk.novopochinati = data.dates[apidate].countries['North Macedonia'].today_new_deaths;
-        this.mk.novoozdraveni = data.dates[apidate].countries['North Macedonia'].today_new_recovered;
-        this.mk.vkupnozaboleni = data.dates[apidate].countries['North Macedonia'].today_confirmed;
-        this.mk.vkupnopochinati = data.dates[apidate].countries['North Macedonia'].today_deaths;
-        this.mk.vkupnoozdraveni = data.dates[apidate].countries['North Macedonia'].today_recovered;
-        this.mk.aktivni = data.dates[apidate].countries['North Macedonia'].today_open_cases;
-        this.mk.date = data.dates[apidate].countries['North Macedonia'].date;
+      });
+  }
 
-        this.svet.novozaboleni = data.total.today_new_confirmed;
-        this.svet.novopochinati = data.total.today_new_deaths;
-        this.svet.novoozdraveni = data.total.today_new_recovered;
-        this.svet.vkupnozaboleni = data.total.today_confirmed;
-        this.svet.vkupnopochinati = data.total.today_deaths;
-        this.svet.vkupnoozdraveni = data.total.today_recovered;
-        this.svet.aktivni = data.total.today_open_cases;
-        this.svet.date = data.total.date;
-      }
-    },
-    (err: any) => {
-      console.log(err);
-      this.nowCD = this.nowCD - 86400000;
-      this.apidate = formatDate(this.nowCD, "yyyy-MM-dd", "en-US");
-      this.retrieveCovidData(this.apidate);           
-  });
+  retrieveCovidDataLast7Days(nowCD?: number) {
+    if(this.mk7days.length > 6) {
+      localStorage.setItem("mk7days", JSON.stringify(this.mk7days));
+      localStorage.setItem("svet7days", JSON.stringify(this.svet7days));
+      return;
+    }
+    this.nowCD = this.nowCD - 86400000;
+    this.apidatel7d = formatDate(this.nowCD, "yyyy-MM-dd", "en-US");
+    this.dataService.getCovidData(this.apidatel7d)
+      .subscribe((data: any) => {
+        let dayMKData = {
+          novozaboleni: data.dates[this.apidatel7d].countries['North Macedonia'].today_new_confirmed,
+          novopochinati: data.dates[this.apidatel7d].countries['North Macedonia'].today_new_deaths,
+          novoozdraveni: data.dates[this.apidatel7d].countries['North Macedonia'].today_new_recovered,
+          vkupnozaboleni: data.dates[this.apidatel7d].countries['North Macedonia'].today_confirmed,
+          vkupnopochinati: data.dates[this.apidatel7d].countries['North Macedonia'].today_deaths,
+          vkupnoozdraveni: data.dates[this.apidatel7d].countries['North Macedonia'].today_recovered,
+          aktivni: data.dates[this.apidatel7d].countries['North Macedonia'].today_open_cases,
+          date: data.dates[this.apidatel7d].countries['North Macedonia'].date
+        };
+        let dayWData = {
+          novozaboleni:data.total.today_new_confirmed,
+          novopochinati: data.total.today_new_deaths,
+          novoozdraveni: data.total.today_new_recovered,
+          vkupnozaboleni: data.total.today_confirmed,
+          vkupnopochinati: data.total.today_deaths,
+          vkupnoozdraveni: data.total.today_recovered,
+          aktivni: data.total.today_open_cases,
+          date: data.total.date
+        };
+        this.mk7days.push(dayMKData);
+        this.svet7days.push(dayWData);
+        this.retrieveCovidDataLast7Days(nowCD);
+        },
+        (err: any) => {
+          console.log(err); 
+      });
   }
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -13,29 +13,39 @@ export class LandingComponent implements OnInit {
   @Input() selectedValue: Korona;
   @Input() mk: Korona;
   @Input() svet: Korona;
+  @Input() mk7days: any[];
+  @Input() svet7days: any[];
   @Input() gradovi: Korona[];
   @Input() date: string;
+  selectedValueDummy: Korona;
   IsExpanded1: boolean;
   IsExpanded2: boolean;
   IsExpanded3: boolean;
   new = [];
   names = [];
-  color =[];
+  color = [];
+  dataTypes = [['novozaboleni','Новозаболени'],['novopochinati','Новопочинати'],['novoozdraveni','Новооздравени'],['vkupnozaboleni','Вкупно заболени'],['vkupnopochinati','Вкупно починати'],['vkupnoozdraveni','Вкупно оздравени'],['vkupnoozdraveni','Вкупно активни случаи']];
+  dataType2 = 'novozaboleni';
+  dataType3 = 'novozaboleni';
   chartInitialized: boolean = false;
   chartInitialized2: boolean = false;
   chartInitialized3: boolean = false;
   chartTypes = [['line','Линиски график'],['bar','Столбест график'],['radar','Радар'],['doughnut','Doughnut график'],['pie','Пита'],['polarArea','Polar area график']];
   chartType = 'bar';
-  chartType2 = 'bar';
-  chartType3 = 'bar';
+  chartType2 = 'line';
+  chartType3 = 'line';
   private mediaMatcher: MediaQueryList = matchMedia(
     `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`
   );
   chart;
+  chart2;
+  chart3;
   @ViewChild('canvas') myCanvas: ElementRef;
   @ViewChild('canvas2') myCanvas2: ElementRef;
   @ViewChild('canvas3') myCanvas3: ElementRef;
   public context: CanvasRenderingContext2D;
+  public context2: CanvasRenderingContext2D;
+  public context3: CanvasRenderingContext2D;
 
   constructor() {}
 
@@ -53,7 +63,13 @@ export class LandingComponent implements OnInit {
 
   ngAfterViewInit(): void {
     setInterval(() => {
-      if(this.gradovi && this.selectedValue && !this.chartInitialized) {
+      if(this.mk && !this.chartInitialized2) {
+        this.reinitiateChart(2);
+      }
+      if(this.svet && !this.chartInitialized3) {
+        this.reinitiateChart(3);
+      }
+      if(this.gradovi && this.selectedValue && !this.chartInitialized && this.myCanvas) {
         this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
         this.chart = new Chart(this.context, {
           type: this.chartType,
@@ -75,6 +91,7 @@ export class LandingComponent implements OnInit {
                 ],
                 borderColor: '#3F51B5',
                 borderWidth: 1,
+                fill: false,
               },
             ],
           },
@@ -97,33 +114,28 @@ export class LandingComponent implements OnInit {
         return;
       }
     },2500);
-    if(this.mk && !this.chartInitialized2) {
-      this.reinitiateChart(2);
-    }
-    if(this.svet && !this.chartInitialized3) {
-      this.reinitiateChart(3);
-    }
   }
 
   reinitiateChart(n) {
     if(n == 1) {
-      console.log("inicijalizacija 1");
       this.chartInitialized = false;
       this.chart.destroy();
       this.ngAfterViewInit();
     }
-    else if(n == 2) {
+    else if(n == 2 && this.mk7days.length == 7 && this.myCanvas2) {
       this.chartInitialized2 = false;
-      console.log("inicijalizacija 2");
-      this.context = (<HTMLCanvasElement>this.myCanvas2.nativeElement).getContext('2d');
-      this.chart = new Chart(this.context, {
+      if(this.chart2) {
+        this.chart2.destroy();
+      }
+      this.context2 = (<HTMLCanvasElement>this.myCanvas2.nativeElement).getContext('2d');
+      this.chart2 = new Chart(this.context2, {
         type: this.chartType2,
         data: {
-          labels: ['Новозаразени во Македонија', 'Новопочинати во Македонија', 'Новооздравени во Македонија', 'Активни случаи во Македонија', 'Вкупно заболени во Македонија', 'Вкупно починати во Македонија', 'Вкупно оздравени во Македонија'],
+          labels: [this.mk7days[0].date, this.mk7days[1].date, this.mk7days[2].date, this.mk7days[3].date, this.mk7days[4].date, this.mk7days[5].date, this.mk7days[6].date],
           datasets: [
             {
-              label: 'Коронавирус во Македонија',
-              data: [1,2,3,4,5,6,7],
+              label: 'Коронавирус во Македонија во последните 7 дена',
+              data: [this.mk7days[0][this.dataType2], this.mk7days[1][this.dataType2], this.mk7days[2][this.dataType2], this.mk7days[3][this.dataType2], this.mk7days[4][this.dataType2], this.mk7days[5][this.dataType2], this.mk7days[6][this.dataType2]],
               backgroundColor: [
                 this.randomColorGenerator(),
                 this.randomColorGenerator(),
@@ -135,6 +147,7 @@ export class LandingComponent implements OnInit {
               ],
               borderColor: '#3F51B5',
               borderWidth: 1,
+              fill: false,
             },
           ],
         },
@@ -156,18 +169,20 @@ export class LandingComponent implements OnInit {
       this.chartInitialized2 = true;
       return;
     }
-    else if(n == 3) {
+    else if(n == 3 && this.svet7days.length == 7 && this.myCanvas3) {
       this.chartInitialized3 = false;
-      console.log("inicijalizacija 3");
-      this.context = (<HTMLCanvasElement>this.myCanvas3.nativeElement).getContext('2d');
-      this.chart = new Chart(this.context, {
+      if(this.chart3) {
+        this.chart3.destroy();
+      }
+      this.context3 = (<HTMLCanvasElement>this.myCanvas3.nativeElement).getContext('2d');
+      this.chart3 = new Chart(this.context3, {
         type: this.chartType3,
         data: {
-          labels: ['Новозаразени во Македонија', 'Новопочинати во Македонија', 'Новооздравени во Македонија', 'Активни случаи во Македонија', 'Вкупно заболени во Македонија', 'Вкупно починати во Македонија', 'Вкупно оздравени во Македонија'],
+          labels: [this.svet7days[0].date, this.svet7days[1].date, this.svet7days[2].date, this.svet7days[3].date, this.svet7days[4].date, this.svet7days[5].date, this.svet7days[6].date],
           datasets: [
             {
-              label: 'Коронавирус во светот',
-              data: [1,2,3,4,5,6,7],
+              label: 'Коронавирус во светот во последните 7 дена',
+              data: [this.svet7days[0][this.dataType3], this.svet7days[1][this.dataType3], this.svet7days[2][this.dataType3], this.svet7days[3][this.dataType3], this.svet7days[4][this.dataType3], this.svet7days[5][this.dataType3], this.svet7days[6][this.dataType3]],
               backgroundColor: [
                 this.randomColorGenerator(),
                 this.randomColorGenerator(),
@@ -179,6 +194,7 @@ export class LandingComponent implements OnInit {
               ],
               borderColor: '#3F51B5',
               borderWidth: 1,
+              fill: false,
             },
           ],
         },
@@ -216,8 +232,10 @@ export class LandingComponent implements OnInit {
       this.chartInitialized = false;
     } else if (n == 2) {
       this.IsExpanded2 = !this.IsExpanded2;
+      this.chartInitialized2 = false;
     } else if (n == 3) {
       this.IsExpanded3 = !this.IsExpanded3;
+      this.chartInitialized3 = false;
     }
   }
 }
